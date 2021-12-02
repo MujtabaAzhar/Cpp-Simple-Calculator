19bd0910562220bcf32db3ee67e73fa23f8fbf87
MujtabaAzhar <95415049+MujtabaAzhar@users.noreply.github.com>
2021-12-02 16:12:33 +0500

    Add files via upload
    Started Working on Calculator

---
+++ CALCULATOR.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter first Number to add: ";
+	cin>>a;
+	cout<<"Enter Second Number to add: ";
+	cin>>b;
+	a=a+b;
+	cout<<"Sum is: "<<a;
+}
