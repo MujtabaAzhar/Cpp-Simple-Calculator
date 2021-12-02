428b1591a85c6edcc9eab1f5f81465c00e5db83b
MujtabaAzhar <95415049+MujtabaAzhar@users.noreply.github.com>
2021-12-02 16:18:06 +0500

    Subraction
    Subtraction is Added

--- CALCULATOR.cpp
+++ CALCULATOR.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main()
 {
-	int a,b;
+	int a,b,add,sub;
 	cout<<"Enter first Number to add: ";
 	cin>>a;
 	cout<<"Enter Second Number to add: ";
 	cin>>b;
-	a=a+b;
-	cout<<"Sum is: "<<a;
+	add=a+b;
+	sub=a-b;
+	cout<<"Sum is: "<<add<<endl;
+	cout<<"Subtraction is: "<<sub;
 }
